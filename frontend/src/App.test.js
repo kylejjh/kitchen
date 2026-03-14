f5c01e5c5d072920fcef15f066b2eeedcf5084ed
Kyle-jia <jj3708@nyu.edu>
2026-03-13 20:05:40 -0400

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,20 @@ import App from "./App";
 
 test("renders Kitchen API Demo heading", () => {
   render(<App />);
-  const heading = screen.getByText(/Kitchen API Demo/i);
-  expect(heading).toBeInTheDocument();
+  expect(screen.getByText(/Kitchen API Demo/i)).toBeInTheDocument();
+});
+
+test("renders /demo/one endpoint heading", () => {
+  render(<App />);
+  expect(screen.getByText("/demo/one")).toBeInTheDocument();
+});
+
+test("renders /demo/two endpoint heading", () => {
+  render(<App />);
+  expect(screen.getByText("/demo/two")).toBeInTheDocument();
+});
+
+test("renders /demo/three endpoint heading", () => {
+  render(<App />);
+  expect(screen.getByText("/demo/three")).toBeInTheDocument();
 });
